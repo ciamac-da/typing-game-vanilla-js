c746cfc4809d37d8ce2954823531c411c705c5a0
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-03 14:13:15 +0200

    verfied by Marti4n v1.0.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const word = document.getElementById("word")
 const text = document.getElementById("text")
 const scoreEl = document.getElementById("score")
 const timeEl = document.getElementById("time")
-const endgameEl = document.getElementById("end-game")
+const endgameEl = document.getElementById("end-game-container")
 const settingsBtn = document.getElementById("setting-btn")
 const settings = document.getElementById("settings")
 const settingsForm = document.getElementById("settings-form")
@@ -50,6 +50,9 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text on start
+text.focus();
+
 // Generate random word
 const getRandomWord = () => {
   return words[Math.floor(Math.random() * words.length)]
@@ -69,12 +72,38 @@ const updateScore = () => {
   scoreEl.innerHTML = score
 }
 
+// Update time
+const updateTime = () => {
+  time--
+  timeEl.innerHTML = time + "s"
+  if(time === 0) {
+    clearInterval(timeInterval)
+    // End Game
+    gameOver()
+  }
+}
+// Strat counting down
+const timeInterval = setInterval(updateTime, 1000)
+
+// Game over , show end screen
+const gameOver = () => {
+  endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p>Your final score is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `
+  endgameEl.style.display = "flex"
+}
+
 // Event listeners
 text.addEventListener("input", e => {
 const insertedText = e.target.value
 if(insertedText === randomWord) {
   addWordToDOM()
   updateScore()
-  text.value = ""
+  e.target.value = ""
+
+  time += 5
+  updateTime()
 }
 })
